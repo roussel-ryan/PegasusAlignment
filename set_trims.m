933953c3e101498963587ee2b67436e3fcd46480
scawsome <roussel@ucla.edu>
2020-02-27 16:59:16 -0600

    Update set_trims

--- set_trims.m
+++ set_trims.m
@@ -1,5 +1,18 @@
 function [] = set_trims(trim_values)
 %DO_SCAN Set trim values
-%   trim_values has the form [t1_x t1_y t2_x t2_y]
+%  trim_values has the form [t1_x t1_y t2_x t2_y]v
+%   Open database connection and writes to ML_control the steering magnet values
+%   External control of the steering magnets must be enable on Pegasus Control VI
+   
+    conn=database('pegasus','pegasus','3l3ctr0ns'); 
+    tablename = 'ml_control';
+    colnames={'h2','v2','h3','v3'};
+    data = {trim_values(1), trim_values(2), trim_values(3), trim_values(4)};
+    whereclause = '';
+    update(conn,tablename,colnames,data,whereclause)
 
+    sqlquery = 'SELECT h2,v2,h3,v3 FROM ml_control';
+    results = fetch(conn,sqlquery);
+    cell2mat(results)
+    close(conn)
 end
