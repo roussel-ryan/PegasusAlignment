642da7cceba05963eaca1dfe0e84ae41049c3f36
scawsome <roussel@ucla.edu>
2020-02-27 16:57:09 -0600

    Update get_data

--- get_data.m
+++ get_data.m
@@ -3,6 +3,14 @@ function [data] = get_data()
 %   data should be in the form [t1_x t1_y t2_x t2_y centroid_x centroid_y]
 %   if the beam is no longer on the screen return Nan for
 %   centroid_x,centroid_y
-    data = [1 1 1 1 1 1];
+    conn=database('pegasus','pegasus','3l3ctr0ns'); 
+    tableStr = ' pimax ';
+    tableStr2 = ' pegmagnets_new ';
+    datacc = cell2mat(fetch(conn,strcat('SELECT rudyshot,xc,yc FROM ',tableStr, ' ORDER BY rudyshot DESC LIMIT 1;')));
+    sqlquery = strcat('SELECT steering_h2,steering_v2,steering_h3, steering_v3 ',...
+                      ' FROM pegmagnets_new WHERE rudyshot = ',num2str(datacc(1)), ';'); 
+    results = fetch(conn,sqlquery);
+    datamag = cell2mat(results);
+    data = [datamag datacc(2) datacc(3)]
+    close(conn)
 end
-
